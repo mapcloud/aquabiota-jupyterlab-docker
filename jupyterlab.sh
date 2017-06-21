d4fc3faf08d0fc9f310a15f04ba1f5aa06d4476d
José Beltrán <jose.beltran@aquabiota.se>
2017-06-21 11:25:52 +0200

    Added --allow-root

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #  exec jupyter lab --ip=* --port=8888 --no-browser --notebook-dir=/opt/notebooks
-exec jupyter lab --port=8888 --no-browser --notebook-dir=/opt/notebooks
+exec jupyter lab --port=8888 --no-browser --allow-root --notebook-dir=/opt/notebooks
