7654d7bbd5aeda6dc00e4bc6bb3e9f31652ffc0d
José Beltrán <jose.beltran@aquabiota.se>
2017-06-21 11:35:04 +0200

    changed port to listen on 8899 and ip=*

--- jupyterlab.sh
+++ jupyterlab.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 #  exec jupyter lab --ip=* --port=8888 --no-browser --notebook-dir=/opt/notebooks
-exec jupyter lab --port=8888 --no-browser --notebook-dir=/opt/notebooks
+exec jupyter lab --ip=* --port=8899 --no-browser --notebook-dir=/opt/notebooks
